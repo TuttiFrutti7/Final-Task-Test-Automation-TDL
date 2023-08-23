6a5f634ea3630d90fda6900f9c9f7203641c1849
tuttifrutti7 <daces777@gmail.com>
2023-08-23 22:22:16 +0300

    added base class (TestBase)

---
+++ src/main/java/common/TestBase.java
@@ -0,0 +1,55 @@
+package common;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+import java.io.*;
+import java.util.concurrent.*;
+
+public class TestBase {
+    private ConfigFileReader configFileReader = new ConfigFileReader();
+    private final String url = configFileReader.getUrl();
+    private static WebDriver driver;
+
+    public ConfigFileReader getConfig() {
+        return this.configFileReader;
+    }
+    public void openUrl() {
+        getDriver().get(url);
+    }
+
+    public WebDriver getDriver() {
+        if (this.driver == null) {
+            System.setProperty("webdriver.chrome.driver", "src" + File.separator + "main" + File.separator + "resources" + File.separator + "drivers" + File.separator + "chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        }
+        return this.driver;
+    }
+
+    public void closeDriver() {
+        getDriver().close();
+        getDriver().quit();
+    }
+
+    public WebElement getWebElement(String locator) {
+        if (locator.startsWith("id=")) {
+            String idLocator = locator.substring(3);
+            return getDriver().findElement(By.id(idLocator));
+        } else if (locator.startsWith("name=")) {
+            String nameLocator = locator.substring(5);
+            return getDriver().findElement(By.name(nameLocator));
+        } else if (locator.startsWith("xpath=")) {
+            String xpathLocator = locator.substring(6);
+            return getDriver().findElement(By.xpath(xpathLocator));
+        } else if (locator.startsWith("css=")) {
+            String cssLocator = locator.substring(4);
+            return getDriver().findElement(By.cssSelector(cssLocator));}
+        else if (locator.startsWith("class=")) {
+            String classLocator = locator.substring(6);
+            return getDriver().findElement(By.className(classLocator));
+        }
+        else return null;
+    }
+}
